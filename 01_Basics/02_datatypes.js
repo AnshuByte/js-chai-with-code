73c9153c4a2055985eb9e0110c8adbf3e240f5c0
Anshu Gupta <112775588+anshurauniyar17@users.noreply.github.com>
2023-12-08 16:10:02 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use  strict" //treat all js code as Newer version .
+
+//alert (3+3) // we are using nodejs not browser .so do not work.
+// Code readiliblity should be high
+
+/*
+Eg : 
+console.log(2
+    + 2
+    )
+console.log("anshu"); console.log(2)
+
+Good code :(readable)
+
+console.log("Anshu")
+console.log(5+5)
+*/
+let Name = "Anshu" //string
+let Age = 20 // int
+let IsTrue = false // boolean
+
+
+/*
+primitive data types
+
+Numbers => 2^23
+Bigint
+strings => ""
+boolean => True / False
+null              //it is defined as object
+undefined
+symbols
+
+*/
+console.log(typeof Age)  //number
+console.log(typeof null) // it return type object.
+console.log(typeof Name) // string
+console.log(typeof IsTrue) // boolean
+console.log(typeof undefined) // undefined
