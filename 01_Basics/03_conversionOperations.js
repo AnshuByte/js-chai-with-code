ec89b66793b960867710c97ded94696125328df4
Anshu Gupta <112775588+anshurauniyar17@users.noreply.github.com>
2023-12-08 17:00:43 +0000

    03_conversionOperation

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "33aaa"
+
+console.log(typeof score)
+
+let numScore = Number(score)
+console.log(typeof numScore)
+console.log(numScore)
+
+// "33" => 33
+// undefined => NaN (Not A Number)
+// null => 0
+// true=> 1 false => 0
+
+let isLoggedIn = ""
+
+let boolLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolLoggedIn)
+console.log(boolLoggedIn)
+
+// 1 => True
+// 0 => False
+// "" => False
+// "anshu" => True
+
+let stringno = 30
+console.log(stringno)
+console.log(typeof stringno)
+let stringg = String(stringno)
+console.log(stringg)
+console.log(typeof stringg)
+
+//converts every datatypes to string
