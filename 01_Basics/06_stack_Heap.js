9e634e64d50b10b8d4e71628af23f3357117d1fe
Anshu Gupta <112775588+anshurauniyar17@users.noreply.github.com>
2023-12-24 21:15:41 +0530

    stack_Heap

---
+++ 01_Basics/06_stack_Heap.js
@@ -0,0 +1,23 @@
+// Stack (primitive type)  // Heap (Non- Primitive type)
+
+let myName = "AnshuGupta"
+
+let nickName = myName
+
+nickName = "Anshu"
+
+// a value is copied 
+console.log(myName);
+console.log(nickName);
+
+// it reference to same 
+let userOne = {
+    email : "Anshu@gmail.com",
+    class : "upper"
+}
+
+let usertwo = userOne
+
+usertwo.email = "Ans@gmail.comm"
+
+console.log(userOne.email)
