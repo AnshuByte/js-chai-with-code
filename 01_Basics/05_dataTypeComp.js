a0af86ad712473c2c9ff834a09b1187921c64c36
Anshu Gupta <112775588+AnshuByte@users.noreply.github.com>
2024-01-27 15:15:29 +0530

    Update 05_dataTypeComp.js

--- 01_Basics/05_dataTypeComp.js
+++ 01_Basics/05_dataTypeComp.js
@@ -2,22 +2,22 @@ console.log(2 > 1); //true
 console.log(2>=2); //true
 
 
-// console.log( "2" > 1); //true
-// console.log("02" > 1); // true
+ console.log( "2" > 1); //true
+console.log("02" > 1); // true
 
 // Avoide these type of commparisions
 
-// console.log(null < 0); //false
-// console.log(null == 0); //false
-// console.log(null > 0); //false
-// console.log(null >= 0); //true  
+ console.log(null < 0); //false
+ console.log(null == 0); //false
+ console.log(null > 0); //false
+console.log(null >= 0); //true  
 // /*the reason that an equality checkk == and comparisions works diffrently
 // comparision convert null to number treeating it as 0 . Thats why (4) null>=0 is true and null>0 is false*/
 
-// console.log(undefined < 0); //false
-// console.log(undefined == 0); //false
-// console.log(undefined > 0); //false
-// console.log(undefined >= 0); //false 
+console.log(undefined < 0); //false
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined >= 0); //false 
 
 // Avoide these types of Comparisions *******
 
